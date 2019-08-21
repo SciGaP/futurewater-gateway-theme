b6f4c9dfaec789f30cffb4a82eee4a27dc206b67
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-08-21 02:24:35 -0400

    Changing the theme for futurewater gateway

--- partials/header.blade.php
+++ partials/header.blade.php
@@ -1,7 +1,7 @@
 <title>Nanoconfinement Science Gateway</title>
 <link media="all" type="text/css" rel="stylesheet" href="https://assets.iu.edu/brand/3.x/brand.css">
 <link media="all" type="text/css" rel="stylesheet" href="https://assets.iu.edu/web/3.x/css/iu-framework.min.css">
-<link media="all" type="text/css" rel="stylesheet" href="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/css/nano.css"/>
+<link media="all" type="text/css" rel="stylesheet" href="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/css/futurewater.css"/>
 
 <script src="https://assets.iu.edu/web/1.5/libs/modernizr.min.js"></script>
 <style>
@@ -14,10 +14,10 @@
     <div class="row pad">
         <img src="//assets.iu.edu/brand/3.x/trident-large.png" alt="IU" />
         <p id="iu-campus">
-            <a href="https://nanoconfinement.sciencegateways.iu.edu/" title="Nanoconfinement Science Gateway">
-                <span id="campus-name" class="show-on-desktop" itemprop="name">Nanoconfinement Science Gateway</span>
-                <span class="show-on-tablet" itemprop="name">Nanoconfinement Science Gateway</span>
-                <span class="show-on-mobile" itemprop="name">Nanoconfinement</span>
+            <a href="https://nanoconfinement.sciencegateways.iu.edu/" title="FutureWater Science Gateway">
+                <span id="campus-name" class="show-on-desktop" itemprop="name">FutureWater Science Gateway</span>
+                <span class="show-on-tablet" itemprop="name">FutureWater Science Gateway</span>
+                <span class="show-on-mobile" itemprop="name">FutureWater</span>
             </a>
         </p>
     </div>
