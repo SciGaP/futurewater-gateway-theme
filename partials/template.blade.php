1b066eb8d8e7c03187cdc1539caebe58e8dae7cc
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-08-21 02:56:51 -0400

    Adding content in to futurewater home page

--- partials/template.blade.php
+++ partials/template.blade.php
@@ -58,7 +58,9 @@
                     Gateway developers can thus concentrate their efforts on building their scientific communities and not worry about operations.
                     Science Gateway Platform as a Service (SciGaP) provides application programmer interfaces (APIs) to hosted generic infrastructure services that can be used by domain science
                     communities to create Science Gateways.</p>
+                    <p>
                     <a href="https://scigap.org/" target="_blank">Learn More</a>
+                    </p>
                 </div>
             </div>
             <div class="one-half">
@@ -66,7 +68,7 @@
                     <p class="title">Apache Airavata</p>
                     <p>Apache Airavata is a software framework which is dominantly used to build Web-based science gateways and assist to compose, manage, execute and monitor large scale applications and workflows on distributed computing resources such as local clusters, supercomputers, national grids, academic and commercial clouds.
                     Airavata mainly supports long running applications and workflows on distributed computational resources.</p>
-                    <p>  
+                    <p>
                     <a href="https://airavata.apache.org/" target="_blank">Learn More</a>
                     </p>
                 </div>
